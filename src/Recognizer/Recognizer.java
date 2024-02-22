3afb5153fe0cedeb5c2b65fc004d160a4888b761
006ADD <zhakypbekov.adilet@bk.ru>
2024-02-22 19:47:59 +0600

    third commmit

---
+++ src/Recognizer/Recognizer.java
@@ -0,0 +1,317 @@
+
+package Recognizer;
+
+//import Capture.Capture;
+import Util.ConnectBanco;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import javax.imageio.ImageIO;
+import javax.swing.SwingWorker;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imencode;
+import org.bytedeco.opencv.global.opencv_imgproc;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+
+public class Recognizer extends javax.swing.JFrame {
+
+    private Recognizer.DaemonThread myThread = null; 
+    
+    //JavaCV
+    VideoCapture webSource = null;
+    Mat cameraImage = new Mat();
+    CascadeClassifier cascade = new CascadeClassifier("C:\\photos\\haarcascade_frontalface_alt.xml");
+    FaceRecognizer recognizer = LBPHFaceRecognizer.create();
+    BytePointer mem = new BytePointer();
+    RectVector delectedFaces = new RectVector();
+    
+    //Vars
+    String root, firstNamePerson, lasrNamePerson, officePerson, dobPerson;
+    int idPerson;
+    
+    
+    //Utils
+    ConnectBanco connect = new ConnectBanco();
+   
+    public Recognizer() {
+        initComponents();
+        recognizer.read("C:\\photos\\classifierLBPH.yml");
+        recognizer.setThreshold(80);
+        startCamera();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        label_photo = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        labelOffice = new javax.swing.JLabel();
+        label_name = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Security System - Recognizer");
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        label_photo.setForeground(new java.awt.Color(240, 240, 240));
+        label_photo.setText("jLabel2");
+        label_photo.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(200, 200, 200)));
+        jPanel1.add(label_photo, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 310, 310));
+
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        labelOffice.setBackground(new java.awt.Color(92, 96, 209));
+        labelOffice.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        labelOffice.setForeground(new java.awt.Color(255, 255, 255));
+        labelOffice.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelOffice.setText("Office");
+        labelOffice.setOpaque(true);
+        jPanel2.add(labelOffice, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, 290, 50));
+
+        label_name.setBackground(new java.awt.Color(92, 96, 209));
+        label_name.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        label_name.setForeground(new java.awt.Color(255, 255, 255));
+        label_name.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        label_name.setText("First - Lastname");
+        label_name.setOpaque(true);
+        jPanel2.add(label_name, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 290, 50));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 330, 310, 140));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 508, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        setSize(new java.awt.Dimension(357, 528));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Recognizer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Recognizer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Recognizer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Recognizer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Recognizer().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel labelOffice;
+    private javax.swing.JLabel label_name;
+    private javax.swing.JLabel label_photo;
+    // End of variables declaration//GEN-END:variables
+
+   class DaemonThread implements Runnable {
+
+        protected volatile boolean runnable = false;
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                while (runnable) {
+                    try {
+                        if (webSource.grab()) {
+                            webSource.retrieve(cameraImage);
+                            Graphics g = label_photo.getGraphics();
+
+                            Mat imageGray = new Mat();
+                            cvtColor(cameraImage, imageGray, COLOR_BGRA2GRAY);
+
+                            RectVector detectedFace = new RectVector();
+                            cascade.detectMultiScale(imageGray, detectedFace, 1.1, 2, 0, new Size(150, 150), new Size(500, 500));
+
+                            for (int i = 0; i < detectedFace.size(); i++) {
+                                Rect dadosFace = detectedFace.get(i);
+                                rectangle(cameraImage, dadosFace, new Scalar(0, 255, 0, 0));
+                                Mat faceCapturada = new Mat(imageGray, dadosFace);
+                                opencv_imgproc.resize(faceCapturada, faceCapturada, new Size(160, 160));
+
+                                IntPointer rotulo = new IntPointer(1);
+                                DoublePointer confidence = new DoublePointer(1);
+                                recognizer.predict(faceCapturada, rotulo, confidence);
+                                int prediction = rotulo.get(0);
+                                String nome;
+                                nome = firstNamePerson;
+
+                                if (prediction == -1) {
+                                    /**rectangle(cameraImage, dadosFace, new Scalar(0, 0, 255, 3), 3, 0, 0);
+                                    idPerson = 0;*/
+                                    label_name.setText("Desconhecido");
+                                    labelOffice.setText("");
+                                    idPerson=0;
+                                   
+                                } else {
+                                    //rectangle(cameraImage, dadosFace, new Scalar(0, 255, 0, 3), 3, 0, 0);
+                                    System.out.println(confidence.get(0));
+                                    //idPerson = prediction;
+//                                    System.out.println("PESSOA RECONHECIDA COMO: " + idPerson);
+                                     idPerson = prediction;
+                                    rec();
+                                }
+                            }
+
+                            imencode(".bmp", cameraImage, mem);
+                            Image im = ImageIO.read(new ByteArrayInputStream(mem.getStringBytes()));
+                            BufferedImage buff = (BufferedImage) im;
+
+                            try {
+                                if (g.drawImage(buff, 0, 0, 360, 390, 0, 0, buff.getWidth(), buff.getHeight(), null)) {
+                                    if (runnable == false) {
+                                        this.wait();
+                                    }
+                                }
+                            } catch (Exception e) {
+                            }
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }
+    }
+
+   private void rec(){
+       SwingWorker worker = new SwingWorker(){
+           @Override
+           protected Object doInBackground() throws Exception {
+               connect.conexao();
+               try{
+                   String SQL = "SELECT * FROM person WHERE id = " + String.valueOf(idPerson);
+                   connect.executaSQL(SQL);
+                   while(connect.rs.next()){
+                       label_name.setText(connect.rs.getString("first_name") 
+                               + " " + connect.rs.getString("last_name"));
+                       labelOffice.setText(connect.rs.getString("office"));
+                       
+                       System.out.println("Person: "+ connect.rs.getString("id"));
+                       
+                       java.sql.Array ident = connect.rs.getArray("first_name");
+                       String[] person = (String[]) ident.getArray();
+                       
+                       for(int i=0; i<person.length; i++){
+                           System.out.println(person[i]);
+                       }
+                       
+                   }
+                }catch(Exception e){
+                    
+                }
+               connect.desconecta();
+               return null;
+           }
+       }; worker.execute();
+      /**new Thread() {
+            @Override
+            public void run() {
+                conecta.conexao();
+                try {
+                    conecta.executaSQL("SELECT * FROM person WHERE id = " + String.valueOf(idPerson));
+                    while (conecta.rs.next()) {
+                        firstNamePerson = conecta.rs.getString("first_name");
+                        jLabel10.setText("Hi, " + firstNamePerson + " " + conecta.rs.getString("last_name"));
+                        label_name.setText(conecta.rs.getString("first_name") + " " + conecta.rs.getString("last_name"));
+                        label_office.setText(conecta.rs.getString("office"));
+                        telefone = conecta.rs.getString("phone_number");
+                        label_phone.setText(telefone);
+                        sendMessage_btn.setText("Send Message to " + conecta.rs.getString("phone_number"));
+                        txt_id_label.setText(conecta.rs.getString("id"));
+
+                        //Social Info
+                        facebook = conecta.rs.getString("profile_facebook");
+                        insta = conecta.rs.getString("profile_instagram");
+                        linkedin = conecta.rs.getString("profile_linkedin");
+                        git = conecta.rs.getString("profile_github");
+
+                        Array ident = conecta.rs.getArray("first_name");
+                        String[] person = (String[]) ident.getArray();
+
+                        for (String person1 : person) {
+                            System.out.println(person1);
+                        }
+
+                    }
+                } catch (Exception e) {
+                }
+                conecta.desconecta();
+            }
+        }.start();*/
+    }
+
+    public void stopCamera() {
+        try {
+            myThread.runnable = false;
+            webSource.release();
+            dispose();
+        } catch (Exception e) {
+        }
+    }
+
+    public void startCamera() {
+        new Thread() {
+            @Override
+            public void run() {
+                webSource = new VideoCapture(0);
+                myThread = new Recognizer.DaemonThread();
+                Thread t = new Thread(myThread);
+                t.setDaemon(true);
+                myThread.runnable = true;
+                t.start();
+            }
+        }.start();
+    }
+
+}
